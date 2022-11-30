30a8d6aceedcfda24bad2107835ad1e924de8336
Jacob Fredsøe <fredsoee@hotmail.com>
2022-11-30 23:07:47 +0100

    Inital raw pulling functions done

---
+++ R/pullRequests.R
@@ -0,0 +1,58 @@
+#' Internal function for getting data from the API
+#'
+#' @param name Name of the table
+#' @param mediConnection The API connection
+#' @return A data.frame with the results
+#' @noRd
+getTable = function(name, mediConnection) {
+  url = createURL(name, mediConnection)
+
+  req = curl::curl_fetch_memory(url)
+
+  as.data.frame(jsonlite::fromJSON(rawToChar(req$content)))
+}
+
+#' Gets all logged procedures from MediSkill
+#'
+#' @param mediConnection The API connection
+#' @return A data.frame with the results
+#' @export
+getProcedures = function(mediConnection) {
+  getTable("procedures", mediConnection)
+}
+
+#' Gets all VOT survey responses from MediSkill
+#'
+#' @param mediConnection The API connection
+#' @return A data.frame with the results
+#' @export
+getVOTs = function(mediConnection) {
+  getTable("vots", mediConnection)
+}
+
+#' Gets all OPERATE survey responses from MediSkill
+#'
+#' @param mediConnection The API connection
+#' @return A data.frame with the results
+#' @export
+getOPERATEs = function(mediConnection) {
+  getTable("operates", mediConnection)
+}
+
+#' Gets all EVARATE survey responses from MediSkill
+#'
+#' @param mediConnection The API connection
+#' @return A data.frame with the results
+#' @export
+getEVARATEs = function(mediConnection) {
+  getTable("evarates", mediConnection)
+}
+
+#' Gets all user details from MediSkill
+#'
+#' @param mediConnection The API connection
+#' @return A data.frame with the results
+#' @export
+getUsers = function(mediConnection) {
+  getTable("users", mediConnection)
+}
---
+++ R/settings.R
@@ -0,0 +1,30 @@
+
+#' Creates a connection object for the MediSkill API
+#'
+#' Usage:
+#' mediConn = mediskillConnection('your token here')
+#'
+#' allProcedures = getProcedures(mediConn)
+#'
+#' @param url Base url for the api
+#' @param token The API token
+#' @return A connection object
+#' @export
+mediskillConnection = function (token, url = "https://mediskill.dk/api/") {
+
+  if (is.na(url) && missing(conn))
+    stop("Need one of url or conn")
+  if (!is.na(url)) {
+    if (missing(token))
+      stop("Need an API token")
+    return(structure(list(url = url, token = token, class = "mediskillConnection")))
+  }
+}
+
+
+#' for creating the url string
+#' @noRd
+createURL = function(request, mediConnection) {
+  slash = ifelse(substr(mediConnection$url, nchar(mediConnection$url), nchar(mediConnection$url)) == "/", "", "/")
+  return(paste0(mediConnection$url, slash, request, "?token=", mediConnection$token))
+}
